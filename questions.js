094513f5fd8940be4371bc171e703315adc419e8
arkohors <arkohors@gmail.com>
2016-05-17 21:55:12 -0700

    initial commit

---
+++ questions.js
@@ -0,0 +1,37 @@
+var questions = [{
+{
+	question: "1. What expansion set is this symbol from?"
+	picture: "ARB_symbol.png",
+	answerChoices: ["Alara Reborn", "Dragons of Tarkir", "Khans of Tarkir", "Odyssey"],
+	correctChoice: 0,
+	description:
+},
+{
+	question: "2. What expansion set is this symbol from?"
+	picture: "MOR_symbol.png",
+	answerChoices: ["Alara Reborn", "MorningTide", "Khans of Tarkir", "Odyssey"],
+	correctChoice: 1,
+	description:
+},
+{
+	question: "3. What expansion set is this symbol from?"
+	picture: "ODY_symbol.png",
+	answerChoices: ["Alara Reborn", "Dragons of Tarkir", "Khans of Tarkir", "Odyssey"],
+	correctChoice: 3,
+	description:
+},
+{
+	question: "4. What expansion set is this symbol from?"
+	picture: "LRW_symbol.png",
+	answerChoices: ["Alara Reborn", "Lorwyn", "Scars of Mirrodin", "Odyssey"],
+	correctChoice: 1,
+	description:
+},
+{
+	question: "5. What expansion set is this symbol from?"
+	picture: "SOM_symbol.png",
+	answerChoices: ["Alara Reborn", "Dragons of Tarkir", "Scars of Mirrodin", "Odyssey"],
+	correctChoice: 2,
+	description:
+
+}]
