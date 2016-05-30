0bc5cb459e32dec6d40689dc66d67d3152e55f3c
arkohors <arkohors@gmail.com>
2016-05-29 17:13:26 -0700

    mockup complete

--- questions.js
+++ questions.js
@@ -4,34 +4,39 @@ var questions = [{
 	picture: "ARB_symbol.png",
 	answerChoices: ["Alara Reborn", "Dragons of Tarkir", "Khans of Tarkir", "Odyssey"],
 	correctChoice: 0,
-	description:
+	feedback: "This symbol is from Alara Reborn."
 },
 {
 	question: "2. What expansion set is this symbol from?"
 	picture: "MOR_symbol.png",
 	answerChoices: ["Alara Reborn", "MorningTide", "Khans of Tarkir", "Odyssey"],
 	correctChoice: 1,
-	description:
+	feedback: "This symbolis from MorningTide."
 },
 {
 	question: "3. What expansion set is this symbol from?"
 	picture: "ODY_symbol.png",
 	answerChoices: ["Alara Reborn", "Dragons of Tarkir", "Khans of Tarkir", "Odyssey"],
 	correctChoice: 3,
-	description:
+	feedback: "This symbol is from Odyssey."
 },
 {
 	question: "4. What expansion set is this symbol from?"
 	picture: "LRW_symbol.png",
 	answerChoices: ["Alara Reborn", "Lorwyn", "Scars of Mirrodin", "Odyssey"],
 	correctChoice: 1,
-	description:
+	feedback: "This symbol is from Lorwyn."
 },
 {
 	question: "5. What expansion set is this symbol from?"
 	picture: "SOM_symbol.png",
 	answerChoices: ["Alara Reborn", "Dragons of Tarkir", "Scars of Mirrodin", "Odyssey"],
 	correctChoice: 2,
-	description:
+	feedback: "This symbol is from Scars of Mirrodin."
 
 }]
+
+
+/* Functions needed:
+Generate new questions from Object
+	Includes 
